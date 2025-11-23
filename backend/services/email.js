e08ef7401b845ad1cec6d2518e48847234039350
OakLeaf007 <jjjbbbb736@gmail.com>
2025-11-24 00:37:27 +0530

    major update/otp bugfix

--- backend/services/email.js
+++ backend/services/email.js
@@ -1,25 +1,29 @@
-import nodemailer from "nodemailer";
+
 
 async function sendOtp(email, otp){
-    const transporter = nodemailer.createTransport({
-        host: "smtp-relay.brevo.com",
-        port:587,
-        auth: {
-            user: "9c53b7001@smtp-brevo.com",
-            pass: "LAF9cfYBz1rR5kD6"
-        }
-    });
+   
+const data = {
+    sender:{
+        name: "cornflix",
+        email: "jjjbbbb736@gmail.com"
 
-
-    const mailOptions={
-        from: "cornflix <jjjbbbb736@gmail.com>",
-        to: email,
-        subject: "Password reset OTP",
-        html: `<p> your OTP is:</p>
+    },
+    to:[{email}],
+    subject:"Password reset OTP",
+     htmlContent: `<p> your OTP is:</p>
         <h1 style = "color:blue;">${otp}</h1>
         <p>This will expire in 5 minutes</p>`
+};
+const res = await fetch ("https://api.brevo.com/v3/smtp/email",{
+    method: "POST",
+    headers:{
+        "accept": "application/json",
+        'api-key':"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",
+        "content-type": "application/json"
+    },
+    body: JSON.stringify(data)
+});
+if(!res.ok) throw new Error("Brevo API failed");
 
-    };
-    return transporter.sendMail(mailOptions);
 }
 export default sendOtp;
